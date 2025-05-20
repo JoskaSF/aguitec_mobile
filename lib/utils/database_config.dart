cb9ea32da08703160bcf51ec3faf1fee23d7a7b3
JoskaSF <al22760905@ite.edu.mx>
2025-05-19 21:20:34 -0700

    refactor: remove unused DatabaseConfig class

--- lib/utils/database_config.dart
+++
@@ -1,9 +0,0 @@
-class DatabaseConfig {
-  static const String host = '127.0.0.1';
-  static const int port = 3306;
-  static const String user = 'admin';
-  static const String password = 'admin';
-  static const String database = 'Aguitec';
-}
-
-//Test
